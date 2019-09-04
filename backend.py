a3466168e96ea06ec6bfe3a4d003f0dcc33b31d4
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2019-09-04 18:36:24 +0530

    Updated code with appropriate comments

--- backend.py
+++ backend.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy 
 import uuid
 import jwt
-import json
 import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
@@ -15,7 +14,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///relationships.db'
 
 db = SQLAlchemy(app)
 
-
+# Making the token_required wrapper functionality
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -36,7 +35,7 @@ def token_required(f):
         return f(current_user, *args, **kwargs)
     return decorated
 
-
+# Class student representing a table student with it's various columns having various attributes
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
@@ -47,6 +46,7 @@ class Student(db.Model):
     last_outtime = db.Column(db.DateTime, nullable=True)
     books = db.relationship('Book', backref='assignee')
 
+# Class Book representing a table book
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
@@ -55,7 +55,7 @@ class Book(db.Model):
     issued_at = db.Column(db.DateTime, nullable=True)
     owner_id = db.Column(db.Integer, db.ForeignKey('student.reg_no'), nullable=True)
 
-
+# This route is used to get the list of all students. Primarily required for testing purposes.
 @app.route('/get_students', methods=['GET'])
 @token_required
 def get_students(current_user):
@@ -71,6 +71,7 @@ def get_students(current_user):
 
     return jsonify({'students': students_list})
 
+# This is required to get the details of a book, like it's barcode id, rack no. just from it's name. To be improved upon
 @app.route('/get_book_details/<name>', methods=['GET'])
 @token_required
 def get_book_details(current_user, name):
@@ -85,6 +86,7 @@ def get_book_details(current_user, name):
 
     return jsonify({'books': books_list})
 
+# This returns the list of books issued by the student
 @app.route('/get_books', methods=['GET'])
 @token_required
 def get_books(current_user):
@@ -100,6 +102,7 @@ def get_books(current_user):
 
     return jsonify({'books': books_list})
 
+# This is used to post a student to the library database. To be performed by admins.
 @app.route('/student', methods=['POST'])
 def post_student():
     data = request.get_json()
@@ -109,6 +112,7 @@ def post_student():
 
     return jsonify({'message': 'Student registered!'})
 
+# This is used to add a book to the library database. Also performed by the admins.
 @app.route('/book', methods=['POST'])
 def post_book():
     data = request.get_json()
@@ -119,6 +123,7 @@ def post_book():
 
     return jsonify({'message': 'Book Registered'})
 
+# This is used to register the students through the app. The student has to enter name, reg_no, and password as details.
 @app.route('/register', methods=['PUT'])
 def register():
     data = request.get_json()
@@ -133,23 +138,27 @@ def register():
 
     return jsonify({'message': 'Student registered successfully!'})
 
+# This is used to issue the book through the app. Book can be issued only between entry and exit time.
 @app.route('/issue/<book_id>', methods=['PUT'])
 @token_required
 def book_issue(current_user, book_id):
     book = Book.query.filter_by(barcode_id=book_id, tag=False).first()
 
     current_time = datetime.datetime.utcnow()
-
+    
+    # For first timers
     if current_user.last_intime is None:
         return jsonify({'message': 'Cannot return book'})
         
     if current_user.last_outtime is None:
         if not book:
             return jsonify({'message': 'Cannot return book'})
-
+    
+    # If regular student at library, or at least has come more than once. Checking if student is issuing book in library.
     if not book or ((current_time-current_user.last_intime).total_seconds()<0 or (current_user.last_intime-current_user.last_outtime).total_seconds()<0):
         return jsonify({'message': 'Cannot issue book'})
-
+    
+    # Assigning the book to the student
     book.assignee = current_user
     book.tag = True
     book.issued_at = current_time
@@ -158,6 +167,7 @@ def book_issue(current_user, book_id):
 
     return jsonify({'message': 'Book assigned successfully'})
 
+# Similarly this is for returning the books, subject to similar conditions as in issuing the books.
 @app.route('/return/<book_id>', methods=['PUT'])
 @token_required
 def book_return(current_user, book_id):
@@ -181,34 +191,41 @@ def book_return(current_user, book_id):
 
     return jsonify({'message': 'Book returned successfully'})
 
+# Used by the students to log in their account through the app 
 @app.route('/login')
 def login():
     auth = request.authorization
-
+    
+    # If authorization values are not provided
     if not auth or not auth.username or not auth.password:
         return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm="Login Required!"'})
 
     student = Student.query.filter_by(reg_no=auth.username).first()
-
+    
+    # If student not found in database. Can occur if not registered.
     if not student:
         return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm="Login Required!"'})
 
+    # Generating token which has an expiry time after which student has to log in again
     if check_password_hash(student.password, auth.password):
         token = jwt.encode({'reg_no' : student.reg_no, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
         return jsonify({'token': token.decode('UTF-8')})
 
     return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm="Login Required!"'})
 
+# Route to integrate the entry-exit system. Student just has to scan the rfid card, and will be allowed entry or exit.
 @app.route('/entry_exit', methods=['PUT'])
 def entry_exit():
     data = request.get_json()
     student = Student.query.filter_by(rfid_id=data['rfid_id']).first()
     
+    # If intime is none, then student is a first timer and is entering the library for the first time.
     if student.last_intime is None:
         student.last_intime = datetime.datetime.utcnow()
         db.session.commit()
         return jsonify({'status': 'Entry'})
-
+    
+    # If student is not a first timer
     if student.last_outtime is not None:
         time_diff = student.last_outtime - student.last_intime
 
@@ -219,6 +236,8 @@ def entry_exit():
 
     
     student.last_outtime = datetime.datetime.utcnow()
+    
+    # Checking for books issued by student between his last entry and exit times. They will be displayed on screen.
     books = Book.query.filter_by(assignee=student).all()
     book_list = []
     for book in books:
@@ -230,6 +249,6 @@ def entry_exit():
 
 
 
-
+# Running the application
 if __name__=='__main__':
     app.run(debug=True)
